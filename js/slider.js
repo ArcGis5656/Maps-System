1cd78ed00b040ada60d418dc019206c5ebee0cfa
karama Alnezari <karama.alnezari@gmail.com>
2022-04-20 17:48:23 +0300

    oh allah this is the last

--- js/slider.js
+++ js/slider.js
@@ -21,4 +21,27 @@ $('.same-height div').each(
   }
 );
 $('.same-height div').height(theMax);
+function openMod() {
+  document.getElementById("mymodal").style.width = "250px";
+}
+
+window.addEventListener('click', function(e){   
+if (!document.getElementById('mymodal').contains(e.target) && !document.getElementById('mymod').contains(e.target)){
+  // Clicked in box
+ document.getElementById("mymodal").style.width = "0px";  
+} else{
+ 
+// document.getElementById("mymodal").style.width = "0px";
+}
+});
+function openmod() {
+  document.getElementById("mySidemod").style.width = "250px";
+}
+
+function openLab($id){
+   document.getElementById($id).style.display='block';
+   document.getElementById('labButton').style.display='none';
+}
+
+
 
